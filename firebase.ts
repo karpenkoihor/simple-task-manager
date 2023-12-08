fad928edc3b355bb3a73141ec926d15fb5e39600
Karpenko Ihor <karpenkoihor96@gmail.com>
2023-12-08 15:14:32 +0200

    init simple-task manager

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0-nMSPwlE7MqBb-s9VRFKH7mElk-fDpU",
+  authDomain: "simple-task-manager-ca05a.firebaseapp.com",
+  projectId: "simple-task-manager-ca05a",
+  storageBucket: "simple-task-manager-ca05a.appspot.com",
+  messagingSenderId: "41539293767",
+  appId: "1:41539293767:web:69e50abb9138d1ff915762",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
